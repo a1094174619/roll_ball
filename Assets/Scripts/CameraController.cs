7e78acdab538eed8b47c1ba3fbd293f1a06d5442
QingHui <1094174619@qq.com>
2019-04-23 10:27:11 +0800

    backup

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    public Vector3 offset;
+    void Start()
+    {
+        this.offset = this.transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        this.transform.position = player.transform.position + this.offset;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody rb; 
+    public int count;
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+        this.rb = GetComponent<Rigidbody>();
+    }
+
+
+
+    void FixedUpdate ()
+    {
+        float movehorizontal = Input.GetAxis("Horizontal");
+        float moververtical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(movehorizontal, 0.0f, moververtical);
+
+        this.rb.AddForce(movement * this.speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+        }
+    }
+}
